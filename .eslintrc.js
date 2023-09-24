556cdb7f89c4d23162c782933bc666f0ad9aa3c0
Laxman Desai <lolopaw24@gmail.com>
2023-09-24 17:11:29 +0530

    Remove unsafe injection

--- .eslintrc.js
+++ .eslintrc.js
@@ -84,6 +84,7 @@ module.exports = {
     "react/react-in-jsx-scope": "off",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
+    "security/detect-object-injection": "off",
 
     // ? Prettier rules
     "prettier/prettier": [
